3fdcf32e3d7b7f9227956f22fc5abc961ec3d34e
Gabriela B Rodrigues <betancourth.gab@gmail.com>
2023-06-06 16:38:43 +0200

    users, controller, some views and cloud pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# DESTROYING
+
+puts "Cleaning offers database..."
+Offer.destroy_all
+
+puts "Cleaning hotels database..."
+Hotel.destroy_all
+
+puts "Cleaning flights database..."
+Flight.destroy_all
+
+puts "Cleaning users database..."
+User.destroy_all
+
+# CREATING
+
+puts "Creating 5 new users..."
+5.times do
+  User.create!(email: Faker::Internet.email, password: "123456")
+end
+puts "Creating 5 new offers..."
+5.times do
+  offer = Offer.create!(destination: Faker::Address.city, budget: Faker::Number.decimal(l_digits: 2), start_date: Faker::Date.in_date_period, end_date: Faker::Date.in_date_period, user_id: User.all.sample.id)
+  puts "Creating  new hotels..."
+    Hotel.create!(price: Faker::Number.decimal(l_digits: 2), name: Faker::Coffee.blend_name, address: Faker::Address.street_address, offer_id: offer.id)
+
+  puts "Creating  new flights..."
+    Flight.create!(price: Faker::Number.decimal(l_digits: 2), departure: Faker::Date.in_date_period, arrival: Faker::Date.in_date_period, offer_id: offer.id)
+    Flight.create!(price: Faker::Number.decimal(l_digits: 2), departure: Faker::Date.in_date_period, arrival: Faker::Date.in_date_period, offer_id: offer.id)
+end
