0225609af1ddbe9597ddd02f0b3e45c6eed8a9d6
Charlotte Nirenberg <charloutte19999@gmail.com>
2023-06-06 16:33:08 +0200

    api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "let's create some hotel offer "
+
+url = URI("https://tripadvisor16.p.rapidapi.com/api/v1/hotels/searchHotelsByLocation?latitude=40.730610&longitude=-73.935242&checkIn=%3CREQUIRED%3E&checkOut=%3CREQUIRED%3E&pageNumber=1&currencyCode=USD")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["X-RapidAPI-Key"] = 'b4b3100277mshe0370ca0aea55ddp14b73bjsn4164eba75913'
+request["X-RapidAPI-Host"] = 'tripadvisor16.p.rapidapi.com'
+
+hotel_response = http.request(request)
+puts response.read_body
+
+flight_response = http.request(request).read_body
+flight_response_ok = JSON.parse(response)
+puts "hotel  offer has been created succesfully"
