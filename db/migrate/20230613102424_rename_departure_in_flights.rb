feb6eb1ce841a202c5c59091506bbf046d0b7d73
Gabriela B Rodrigues <betancourth.gab@gmail.com>
2023-06-14 14:13:21 +0200

    API work but bugs to fix

--- db/migrate/20230613102424_rename_departure_in_flights.rb
+++
@@ -1,5 +0,0 @@
-class RenameDepartureInFlights < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :flights, :departure, :departure_outbound
-  end
-end
--- db/migrate/20230613103119_add_return_inbound_in_flights.rb
+++
@@ -1,5 +0,0 @@
-class AddReturnInboundInFlights < ActiveRecord::Migration[7.0]
-  def change
-    add_column :flights, :return_inbound, :datetime
-  end
-end
